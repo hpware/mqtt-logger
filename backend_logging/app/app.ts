851b1d2df6ae516b3ed2cb3af0bd685baee38a5e
吳元皓 <hw@yuanhau.com>
2025-08-10 22:06:21 +0800

    Change dirs

--- backend_logging/app/app.ts
+++ backend_logging/app/app.ts
@@ -11,16 +11,33 @@ mqtt.on("connect", () => {
 
 mqtt.on("reconnect", () => console.log("Reconnecting..."));
 mqtt.on("error", async (err) => {
-  await sql`INSERT INTO mqtt_errors (uuid, errormsg) VALUE (${randomUUIDv7()}, ${err}`;
+  await sql`INSERT INTO errorsys (uuid, source, errormsg) VALUES (${randomUUIDv7()}, "mqtt", ${err})`;
   console.error("MQTT error:", err);
 });
 
-mqtt.on("message", (topic, message) => {
+mqtt.on("message", async (topic, message) => {
   const raw = message.toString();
   const displayed = raw.replace(/\r/g, "\\r").replace(/\n/g, "\\n");
   process.stdout.write(`Topic: ${topic}, Message: ${displayed}\n`);
+
+  let isJson = false;
+  let data = raw;
+  try {
+    JSON.parse(raw);
+    isJson = true;
+    data = JSON.stringify(raw);
+  } catch (e) {
+    isJson = false;
+  }
+  try {
+    await sql`INSERT INTO mqtt_data (uuid, topic, data, isjson)
+             VALUES (${randomUUIDv7()}, ${topic}, ${raw}, ${isJson})`;
+  } catch (e) {
+    console.log(e);
+  }
 });
 
+// ON PROCESS QUIT
 process.on("SIGINT", () => {
   console.log("Closing MQTT connection...");
   mqtt.end(false, {}, () => {
@@ -28,7 +45,6 @@ process.on("SIGINT", () => {
     process.exit(0);
   });
 });
-
 process.on("SIGTERM", () => {
   console.log("Closing MQTT connection...");
   mqtt.end(false, {}, () => {
--- backend_logging/app/init.ts
+++ backend_logging/app/init.ts
@@ -5,13 +5,15 @@ create table if not exists mqtt_data (
 uuid text PRIMARY KEY,
 topic text not null,
 data text not null,
+isjson boolean not null,
 created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
 )
 `;
 
 await sql`
-create table if not exists mqtt_errors (
+create table if not exists errorsys (
 uuid text primary key,
+source text not null,
 errormsg text not null,
 created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
 )
